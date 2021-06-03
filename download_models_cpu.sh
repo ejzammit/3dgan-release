cc68bb81cc433a2382b0b47263625a460a08016e
Edward <ejzammit@gmail.com>
2021-06-03 18:37:11 +0200

    Update download_models_cpu.sh

--- download_models_cpu.sh
+++ download_models_cpu.sh
@@ -1,5 +1,4 @@
 mkdir -p models_cpu
-
 wget http://3dgan.csail.mit.edu/models_cpu/car_G_cpu.t7 -O models_cpu/car_G_cpu.t7
 wget http://3dgan.csail.mit.edu/models_cpu/chair_G_cpu.t7 -O models_cpu/chair_G_cpu.t7
 wget http://3dgan.csail.mit.edu/models_cpu/desk_G_cpu.t7 -O models_cpu/desk_G_cpu.t7
